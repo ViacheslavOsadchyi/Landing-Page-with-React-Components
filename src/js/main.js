edbaec71eef1da4c0dd45f4a005f177d9661842d
Viacheslav Osadchyi <Viacheslav Osadchyi>
2017-07-04 17:10:39 +0300

    Configure basic dev environment: webpack-dev-server / js & css HMR

---
+++ src/js/main.js
@@ -0,0 +1,6 @@
+import main from '../css/main.css';
+
+let hello = ()=>{
+	console.log('hello!');
+};
+hello();
---
+++ webpack/webpack.common.js
@@ -0,0 +1,44 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var webpack = require('webpack');
+
+module.exports = {
+	entry: './src/js/main.js',
+	output: {
+		path: path.resolve(__dirname, '../dist'),
+		filename: '[name].js'
+	},
+	resolve: {
+		modules: ['./node_modules']
+	},
+	module: {
+		loaders: [
+			{
+				test: 	/\.js/,
+				loader: 'babel-loader',
+				exclude: /(node_modules)/
+			},
+			{
+				test: 	/\.css/,
+                loader: ["style-loader", "css-loader", "postcss-loader"]
+			},
+			{
+				test: 	/\.jade/,
+				loader: 'jade-loader'
+			}
+		]
+	},
+	plugins: [
+		new webpack.HotModuleReplacementPlugin(),
+		new HtmlWebpackPlugin({
+			title: 		'TM Test',
+			template: 	'src/html/index.jade'
+		})
+	],
+	devServer: {
+	    hot: true,
+	    inline: true,
+	    contentBase: path.resolve(__dirname, '../dist'),
+	    publicPath: '/'
+  	}
+}
